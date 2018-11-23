82c9174ea17e49cdacefbd1b9f5d1568aa8093e3
Tom <trlc1996@hotmail.com>
2018-11-23 14:21:12 +0000

    Adding SigFox basics, due to lack of ardunio to test the code is put in on faith and it is hoped to be tested later

--- wifi_test/wifi_test.ino
+++ wifi_test/wifi_test.ino
@@ -1,9 +1,14 @@
-#include <Bridge.h>
-#include <HttpClient.h>
+#include <SigFox.h>
+#include <ArduinoLowPower.h>
+#include <RCSwitch.h>
+
+RCSwitch theSwitch = RCSwitch();
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial) {};
+
+  setupSigFox();
   setupRadioWave();
 }
 
@@ -15,10 +20,150 @@ void loop() {
   delay(50000);
 }
 
+void setupSigFox() {
+  // Uncomment this line and comment begin() if you are working with a custom board
+  //if (!SigFox.begin(SPI1, 30, 31, 33, 28, LED_BUILTIN)) {
+  if (!SigFox.begin()) {
+    Serial.println("Shield error or not present!");
+    return;
+  }
+  // Enable debug led and disable automatic deep sleep
+  // Comment this line when shipping your project :)
+  SigFox.debug();
+
+  String version = SigFox.SigVersion();
+  String ID = SigFox.ID();
+  String PAC = SigFox.PAC();
+
+  // Display module informations
+  Serial.println("MKRFox1200 Sigfox first configuration");
+  Serial.println("SigFox FW version " + version);
+  Serial.println("ID  = " + ID);
+  Serial.println("PAC = " + PAC);
+
+  Serial.println("");
+
+  Serial.print("Module temperature: ");
+  Serial.println(SigFox.internalTemperature());
+
+  Serial.println("Register your board on https://backend.sigfox.com/activate with provided ID and PAC");
+
+  delay(100);
+
+  // Send the module to the deepest sleep
+  SigFox.end();
+
+  Serial.println("Type the message to be sent");
+  while (!Serial.available());
+
+  String message;
+  while (Serial.available()) {
+    message += (char)Serial.read();
+  }
+
+  // Every SigFox packet cannot exceed 12 bytes
+  // If the string is longer, only the first 12 bytes will be sent
+
+  if (message.length() > 12) {
+    Serial.println("Message too long, only first 12 bytes will be sent");
+  }
+
+  Serial.println("Sending " + message);
+
+  // Remove EOL
+  message.trim();
+
+  // Example of message that can be sent
+  // sendString(message);
+
+  Serial.println("Getting the response will take up to 50 seconds");
+  Serial.println("The LED will blink while the operation is ongoing");
+
+  // Example of send and read response
+  sendStringAndGetResponse(message);
+}
+
 void setupRadioWave() {
   //This is where the radio wave will be set up
+  theSwitch.enableTransmit(10);
+  mySwitch.setPulseLength(321);
+
+ // set protocol (default is 1, will work for most outlets)
+ // mySwitch.setProtocol(2);
+
+ // Optional set number of transmission repetitions.
+ // mySwitch.setRepeatTransmit(15);
+
+ pinMode(13,OUTPUT);
 }
 
 void radioWave() {
   //Where the main radio bursts will occur
+
+  mySwitch.send("100000101100100011001000");
+ digitalWrite(13,HIGH);
+ delay(500);
+ digitalWrite(13,LOW);
+ delay(10000); 
+}
+void sendString(String str) {
+  // Start the module
+  SigFox.begin();
+  // Wait at least 30mS after first configuration (100mS before)
+  delay(100);
+  // Clears all pending interrupts
+  SigFox.status();
+  delay(1);
+
+  SigFox.beginPacket();
+  SigFox.print(str);
+
+  int ret = SigFox.endPacket();  // send buffer to SIGFOX network
+  if (ret > 0) {
+    Serial.println("No transmission");
+  } else {
+    Serial.println("Transmission ok");
+  }
+
+  Serial.println(SigFox.status(SIGFOX));
+  Serial.println(SigFox.status(ATMEL));
+  SigFox.end();
+}
+
+void sendStringAndGetResponse(String str) {
+  // Start the module
+  SigFox.begin();
+  // Wait at least 30mS after first configuration (100mS before)
+  delay(100);
+  // Clears all pending interrupts
+  SigFox.status();
+  delay(1);
+
+  SigFox.beginPacket();
+  SigFox.print(str);
+
+  int ret = SigFox.endPacket(true);  // send buffer to SIGFOX network and wait for a response
+  if (ret > 0) {
+    Serial.println("No transmission");
+  } else {
+    Serial.println("Transmission ok");
+  }
+
+  Serial.println(SigFox.status(SIGFOX));
+  Serial.println(SigFox.status(ATMEL));
+
+  if (SigFox.parsePacket()) {
+    Serial.println("Response from server:");
+    while (SigFox.available()) {
+      Serial.print("0x");
+      Serial.println(SigFox.read(), HEX);
+    }
+  } else {
+    Serial.println("Could not get any response from the server");
+    Serial.println("Check the SigFox coverage in your area");
+    Serial.println("If you are indoor, check the 20dB coverage or move near a window");
+  }
+  Serial.println();
+
+  SigFox.end();
 }
